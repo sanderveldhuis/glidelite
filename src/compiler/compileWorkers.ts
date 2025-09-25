19fa6cfdb75f63ed7e2f1ac5c53428968a85f01e
sanderveldhuis <mail@sanderveldhuis.nl>
2025-09-25 20:57:12 +0200

    Finalized compile workers

--- src/compiler/compileWorkers.ts
+++ src/compiler/compileWorkers.ts
@@ -22,16 +22,100 @@
  * SOFTWARE.
  */
 
-import { Json } from './types';
+import {
+  existsSync,
+  readdirSync
+} from 'node:fs';
+import { join } from 'node:path';
+import {
+  execute,
+  makeDir,
+  makeFile,
+  readFile,
+  remove
+} from './sysUtils';
+import {
+  ExitStatus,
+  Json
+} from './types';
+
+const regexMinute = '([1-5]?[0-9])'; // Minute 0-59
+const regexMinuteList = '(' + regexMinute + '(-' + regexMinute + '(\\/' + regexMinute + ')?)?)'; // Minute list and range (e.g. 0-4 or 8-12/2)
+const regexHour = '(1?[0-9]|2[0-3])'; // Hour 0-23
+const regexHourList = '(' + regexHour + '(-' + regexHour + '(\\/' + regexHour + ')?)?)'; // Hour list and range (e.g. 0-4 or 8-12/2)
+const dayOfMonth = '([1-9]|[1-2][0-9]|3[0-1])'; // Day of month 1-31
+const dayOfMonthList = '(' + dayOfMonth + '(-' + dayOfMonth + '(\\/' + dayOfMonth + ')?)?)'; // Day of month list and range (e.g. 1-10 or 29-31/2)
+const month = '([1-9]|1[0-2]|jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)'; // Month 1-12, or first three letters of month
+const monthList = '(' + month + '(-' + month + '(\\/' + month + ')?)?)'; // Month list and range (e.g. 1-4 or 8-12/2)
+const dayOfWeek = '([0-7]|mon|tue|wed|thu|fri|sat|sun)'; // Day of week 0-7 or first three letters of day
+const dayOfWeekList = '(' + dayOfWeek + '(-' + dayOfWeek + '(\\/' + dayOfWeek + ')?)?)'; // Day of week list and range (e.g. 0-4 or 5-7/2)
+const any = '(\\*(\\/\\d*)?)'; // Any value and range (e.g. * or */2)
+const regex = `["']glc (task|service) (` +
+  '(@(annually|yearly|monthly|weekly|daily|hourly|reboot))|(' + // Allow @annually, @yearly, @monthly, @weekly, @daily, @hourly, @reboot OR the following following:
+  '(' + regexMinuteList + '(,' + regexMinuteList + ')*|' + any + ') ' + // Single/list minutes, comma separated minutes, or any value (*)
+  '(' + regexHourList + '(,' + regexHourList + ')*|' + any + ') ' + // Single/list hour, comma separated hours, or any value (*)
+  '(' + dayOfMonthList + '(,' + dayOfMonthList + ')*|' + any + ') ' + // Single/list day of month, comma separated days of month, or any value (*)
+  '(' + monthList + '(,' + monthList + ')*|' + any + ') ' + // Single/list month, comma separated months, or any value (*)
+  '(' + dayOfWeekList + '(,' + dayOfWeekList + ')*|' + any + ')' + // Single/list day of week, comma separated days of week, or any value (*)
+  `))["']`;
 
 export function clean(pkg: Json, config: Json, outputDirectory: string): void {
-  console.log(outputDirectory);
+  remove(join(outputDirectory, 'opt', config.name as string, 'workers'));
+  remove(join(outputDirectory, 'etc', 'cron.d', `${config.name as string}_workers`));
 }
 
 export function validate(pkg: Json, config: Json, workingDirectory: string): void {
-  console.log(workingDirectory);
+  const workersTsConfig = join(workingDirectory, 'backend', 'workers', 'tsconfig.json');
+
+  if (!existsSync(workersTsConfig)) {
+    console.error(`error GL${String(ExitStatus.ProjectInvalid)}:`, `No valid project found at: '${workingDirectory}', missing file '${workersTsConfig}'.`);
+    return process.exit(ExitStatus.ProjectInvalid);
+  }
 }
 
 export function compile(pkg: Json, config: Json, workingDirectory: string, outputDirectory: string): void {
-  console.log(outputDirectory);
+  const workersDir = join(workingDirectory, 'backend', 'workers');
+
+  const allFiles = readdirSync(workersDir, { withFileTypes: true, recursive: true });
+  const tsFiles = allFiles.filter(file => new RegExp('.ts$').test(file.name));
+  if (tsFiles.length <= 0) {
+    // Nothing to compile
+    return;
+  }
+
+  const outputDir = join(outputDirectory, 'opt', config.name as string, 'workers');
+  execute(`tsc -p ${workersDir} --outDir ${outputDir}`, workingDirectory);
+
+  let crontab = '';
+  for (const file of tsFiles) {
+    const filePath = join(file.parentPath, file.name);
+    const content = readFile(filePath);
+
+    const gotInstruction = new RegExp(/["']glc .*["']/).exec(content);
+    if (null !== gotInstruction) {
+      const instruction = new RegExp(regex).exec(content);
+      if (null !== instruction) {
+        // TODO: validate crontab rules whether they work with the new directories, also validate what happens when the ${file.parentPath} is the directory itself
+        crontab += `${instruction[2]} root cd /opt/${config.name as string}/workers && node ${file.parentPath}/${file.name.replace(/.ts$/, '.js')} &\n`;
+        if (instruction[1] === 'service') {
+          crontab += `* * * * * root ps aux | grep -v grep | grep -c "node ${file.parentPath}/${file.name.replace(/.ts$/, '.js')}" || cd /opt/${config.name as string}/workers && node ${file.parentPath}/${file.name.replace(/.ts$/, '.js')} &\n`;
+        }
+      }
+      else {
+        console.error(`error GL${String(ExitStatus.ProjectCompileFailed)}:`, `Invalid compiler instruction found in: '${filePath}'.`);
+        return process.exit(ExitStatus.ProjectCompileFailed);
+      }
+    }
+  }
+
+  const packageFile = join(outputDir, 'package.json');
+  const glconfigFile = join(outputDir, 'glconfig.json');
+  makeFile(packageFile, JSON.stringify({ name: config.name, version: config.version, dependencies: pkg.dependencies }));
+  makeFile(glconfigFile, JSON.stringify(config));
+  if (crontab !== '') {
+    const cronDir = join(outputDirectory, 'etc', 'cron.d');
+    const cronFile = join(cronDir, `${config.name as string}_workers`);
+    makeDir(cronDir);
+    makeFile(cronFile, crontab);
+  }
 }
