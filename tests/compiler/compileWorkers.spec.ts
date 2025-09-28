ef9d9de4d6f7140836f768c3ebc212644da9fe05
sanderveldhuis <mail@sanderveldhuis.nl>
2025-09-28 15:56:14 +0200

    Fix failing unittest

--- tests/compiler/compileWorkers.spec.ts
+++ tests/compiler/compileWorkers.spec.ts
@@ -163,7 +163,12 @@ describe('compileWorkers.ts', () => {
 
     // Files with .ts extension available in workers directory, valid instructions found, with dependencies
     // Validating all regexes are not part of this test, it will be done in another test
-    readDir.onCall(5).returns([{ name: 'test1.ts', parentPath: 'input\\backend\\workers\\sub1\\sub2' }, { name: 'test2.ts', parentPath: 'input\\backend\\workers\\sub2\\sub3' }]);
+    if ('win32' === process.platform) {
+      readDir.onCall(5).returns([{ name: 'test1.ts', parentPath: 'input\\backend\\workers\\sub1\\sub2' }, { name: 'test2.ts', parentPath: 'input\\backend\\workers\\sub2\\sub3' }]);
+    }
+    else {
+      readDir.onCall(5).returns([{ name: 'test1.ts', parentPath: 'input/backend/workers/sub1/sub2' }, { name: 'test2.ts', parentPath: 'input/backend/workers/sub2/sub3' }]);
+    }
     readFile.onCall(5).returns('"use strict";\n"glc task @yearly"\nconsole.log("done")').onCall(6).returns('"use strict";\n"glc service"\nconsole.log("done")');
     compile({ name: 'pkg', dependencies: { a: 'b', b: 'c' } }, { name: 'cfg', version: '1.0.0' }, 'input', 'output');
     if ('win32' === process.platform) {
@@ -179,8 +184,8 @@ describe('compileWorkers.ts', () => {
     else {
       sinon.assert.calledWithExactly(readDir.getCall(5), 'input/backend/workers');
       sinon.assert.calledWithExactly(execute.getCall(3), 'tsc -p input/backend/workers --outDir output/opt/cfg/workers', 'input');
-      sinon.assert.calledWithExactly(readFile.getCall(5), 'input\\backend\\workers\\sub1\\sub2/test1.ts');
-      sinon.assert.calledWithExactly(readFile.getCall(6), 'input\\backend\\workers\\sub2\\sub3/test2.ts');
+      sinon.assert.calledWithExactly(readFile.getCall(5), 'input/backend/workers/sub1/sub2/test1.ts');
+      sinon.assert.calledWithExactly(readFile.getCall(6), 'input/backend/workers/sub2/sub3/test2.ts');
       sinon.assert.calledWithExactly(makeFile.getCall(4), 'output/opt/cfg/workers/package.json', '{"name":"cfg","version":"1.0.0","dependencies":{"a":"b","b":"c"}}');
       sinon.assert.calledWithExactly(makeFile.getCall(5), 'output/opt/cfg/workers/glconfig.json', '{"name":"cfg","version":"1.0.0"}');
       sinon.assert.calledWithExactly(makeDir.getCall(0), 'output/etc/cron.d');
