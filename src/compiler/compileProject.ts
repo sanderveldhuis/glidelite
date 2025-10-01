1f5be4ff07ec0a913a491f9cb30030342b3ae884
sanderveldhuis <mail@sanderveldhuis.nl>
2025-10-01 15:58:54 +0200

    Generate install script, package.json, and glconfig.json files in project compiler

--- src/compiler/compileProject.ts
+++ src/compiler/compileProject.ts
@@ -22,8 +22,15 @@
  * SOFTWARE.
  */
 
+import { join } from 'node:path';
 import * as compileWorkers from './compileWorkers';
+import {
+  makeDir,
+  makeFile,
+  remove
+} from './sysUtils';
 import { Json } from './types';
+import { version } from './version';
 
 /**
  * Cleans the project output data from the specified output directory.
@@ -34,7 +41,13 @@ import { Json } from './types';
 export function clean(pkg: Json, config: Json, outputDirectory: string): void {
   // Never remove the complete output directory but only remove the actual generated output data to prevent erasing
   // user data if the user specified an invalid output directory (e.g. the user home folder)
+
+  // Clean each module
   compileWorkers.clean(pkg, config, outputDirectory);
+
+  // Clean the project
+  remove(join(outputDirectory, 'install'));
+  remove(join(outputDirectory, 'opt', config.name as string));
 }
 
 /**
@@ -55,5 +68,41 @@ export function validate(pkg: Json, config: Json, workingDirectory: string): voi
  * @param outputDirectory the output directory where to put the compilation results in
  */
 export function compile(pkg: Json, config: Json, workingDirectory: string, outputDirectory: string): void {
+  // Compile each module
   compileWorkers.compile(pkg, config, workingDirectory, outputDirectory);
+
+  // Create configuration files
+  const optDir = join(outputDirectory, 'opt', config.name as string);
+  const packageFile = join(optDir, 'package.json');
+  const glconfigFile = join(optDir, 'glconfig.json');
+  makeDir(optDir);
+  makeFile(packageFile, JSON.stringify({ name: config.name, version: config.version, dependencies: Object.assign({}, pkg.dependencies, { glidelite: `github:sanderveldhuis/glidelite#v${version}` }) }));
+  makeFile(glconfigFile, JSON.stringify(config));
+
+  // Create install script
+  makeFile(
+    join(outputDirectory, 'install'),
+    '#!/bin/bash\n\n' +
+      'if [ $EUID -ne 0 ]; then\n' +
+      '  echo "This install script should be run as root"\n' +
+      '  exit 1\n' +
+      'fi\n\n' +
+      '# Check if all required packages are installed\n' +
+      'dpkg -s nodejs > /dev/null\n' +
+      'if [ $? -ne 0 ]; then\n' +
+      '  echo "Not all required packages are installed"\n' +
+      '  exit 1\n' +
+      'fi\n\n' +
+      '# Cleanup old project\n' +
+      `rm -rf /etc/cron.d/${config.name as string}_*\n` +
+      `rm -rf /opt/${config.name as string}\n` +
+      `pkill -f "node /opt/${config.name as string}/workers"\n\n` +
+      '# Copy new project\n' +
+      'cp -r opt /\n' +
+      'cp -r etc /\n\n' +
+      '# Install dependencies\n' +
+      `pushd /opt/${config.name as string}\n` +
+      'npm install\n' +
+      'popd\n'
+  );
 }
--- tests/compiler/compileProject.spec.ts
+++ tests/compiler/compileProject.spec.ts
@@ -29,36 +29,83 @@ import {
   validate
 } from '../../src/compiler/compileProject';
 import * as compileWorkersSrc from '../../src/compiler/compileWorkers';
+import * as sysUtilsSrc from '../../src/compiler/sysUtils';
 
 describe('compileProject.ts', () => {
   let cleanWorkers: sinon.SinonStub;
   let compileWorkers: sinon.SinonStub;
   let validateWorkers: sinon.SinonStub;
+  let remove: sinon.SinonStub;
+  let makeDir: sinon.SinonStub;
+  let makeFile: sinon.SinonStub;
 
   beforeEach(() => {
     cleanWorkers = sinon.stub(compileWorkersSrc, 'clean');
     compileWorkers = sinon.stub(compileWorkersSrc, 'compile');
     validateWorkers = sinon.stub(compileWorkersSrc, 'validate');
+    remove = sinon.stub(sysUtilsSrc, 'remove');
+    makeDir = sinon.stub(sysUtilsSrc, 'makeDir');
+    makeFile = sinon.stub(sysUtilsSrc, 'makeFile');
   });
 
   afterEach(() => {
     cleanWorkers.restore();
     compileWorkers.restore();
     validateWorkers.restore();
+    remove.restore();
+    makeDir.restore();
+    makeFile.restore();
   });
 
   it('validate checking the project', () => {
-    validate({ name: 'cfg' }, { name: 'cfg' }, 'input');
-    sinon.assert.calledOnceWithExactly(validateWorkers, { name: 'cfg' }, { name: 'cfg' }, 'input');
+    validate({ name: 'pkg' }, { name: 'cfg' }, 'input');
+    sinon.assert.calledOnceWithExactly(validateWorkers, { name: 'pkg' }, { name: 'cfg' }, 'input');
   });
 
   it('validate cleaning the project', () => {
-    clean({ name: 'cfg' }, { name: 'cfg' }, 'output');
-    sinon.assert.calledOnceWithExactly(cleanWorkers, { name: 'cfg' }, { name: 'cfg' }, 'output');
+    clean({ name: 'pkg' }, { name: 'cfg' }, 'output');
+    sinon.assert.calledOnceWithExactly(cleanWorkers, { name: 'pkg' }, { name: 'cfg' }, 'output');
+    if ('win32' === process.platform) {
+      sinon.assert.calledWithExactly(remove.getCall(0), 'output\\install');
+      sinon.assert.calledWithExactly(remove.getCall(1), 'output\\opt\\cfg');
+    }
+    else {
+      sinon.assert.calledWithExactly(remove.getCall(0), 'output/install');
+      sinon.assert.calledWithExactly(remove.getCall(1), 'output/opt/cfg');
+    }
   });
 
   it('validate compiling the project', () => {
-    compile({ name: 'cfg' }, { name: 'cfg' }, 'input', 'output');
-    sinon.assert.calledOnceWithExactly(compileWorkers, { name: 'cfg' }, { name: 'cfg' }, 'input', 'output');
+    // No dependencies available
+    compile({ name: 'pkg' }, { name: 'cfg', version: '1.0.0' }, 'input', 'output');
+    sinon.assert.calledOnceWithExactly(compileWorkers, { name: 'pkg' }, { name: 'cfg', version: '1.0.0' }, 'input', 'output');
+    if ('win32' === process.platform) {
+      sinon.assert.calledOnceWithExactly(makeDir, 'output\\opt\\cfg');
+      sinon.assert.calledWithExactly(makeFile.getCall(0), 'output\\opt\\cfg\\package.json', '{"name":"cfg","version":"1.0.0","dependencies":{"glidelite":"github:sanderveldhuis/glidelite#v1.0.0"}}');
+      sinon.assert.calledWithExactly(makeFile.getCall(1), 'output\\opt\\cfg\\glconfig.json', '{"name":"cfg","version":"1.0.0"}');
+      sinon.assert.calledWith(makeFile.getCall(2), 'output\\install'); // Not validating the content of the make file
+    }
+    else {
+      sinon.assert.calledOnceWithExactly(makeDir, 'output/opt/cfg');
+      sinon.assert.calledWithExactly(makeFile.getCall(0), 'output/opt/cfg/package.json', '{"name":"cfg","version":"1.0.0","dependencies":{"glidelite":"github:sanderveldhuis/glidelite#v1.0.0"}}');
+      sinon.assert.calledWithExactly(makeFile.getCall(1), 'output/opt/cfg/glconfig.json', '{"name":"cfg","version":"1.0.0"}');
+      sinon.assert.calledWith(makeFile.getCall(2), 'output/install'); // Not validating the content of the make file
+    }
+
+    // Dependencies available
+    compile({ name: 'pkg', dependencies: { hello: 'world', glidelite: '0.9.0' } }, { name: 'cfg', version: '1.0.0' }, 'input', 'output');
+    sinon.assert.calledWithExactly(compileWorkers.getCall(1), { name: 'pkg', dependencies: { hello: 'world', glidelite: '0.9.0' } }, { name: 'cfg', version: '1.0.0' }, 'input', 'output');
+    if ('win32' === process.platform) {
+      sinon.assert.calledWithExactly(makeDir.getCall(1), 'output\\opt\\cfg');
+      sinon.assert.calledWithExactly(makeFile.getCall(3), 'output\\opt\\cfg\\package.json', '{"name":"cfg","version":"1.0.0","dependencies":{"hello":"world","glidelite":"github:sanderveldhuis/glidelite#v1.0.0"}}');
+      sinon.assert.calledWithExactly(makeFile.getCall(4), 'output\\opt\\cfg\\glconfig.json', '{"name":"cfg","version":"1.0.0"}');
+      sinon.assert.calledWith(makeFile.getCall(5), 'output\\install'); // Not validating the content of the make file
+    }
+    else {
+      sinon.assert.calledWithExactly(makeDir.getCall(1), 'output/opt/cfg');
+      sinon.assert.calledWithExactly(makeFile.getCall(3), 'output/opt/cfg/package.json', '{"name":"cfg","version":"1.0.0","dependencies":{"hello":"world","glidelite":"github:sanderveldhuis/glidelite#v1.0.0"}}');
+      sinon.assert.calledWithExactly(makeFile.getCall(4), 'output/opt/cfg/glconfig.json', '{"name":"cfg","version":"1.0.0"}');
+      sinon.assert.calledWith(makeFile.getCall(5), 'output/install'); // Not validating the content of the make file
+    }
   });
 });
